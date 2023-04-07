8cb04ca0f7259568051549c67817e50868055a77
preet.sutariya <59480837+the-pythor@users.noreply.github.com>
2023-04-07 21:00:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,39 @@
 function calculate() {
-  const clockIn = document.getElementById("clockIn").value;
-  const clockOut = document.getElementById("clockOut").value;
+  const clockInOut = document.getElementById("clockInOut").value;
   const timeToLeave = document.getElementById("timeToLeave");
   
-  if (clockIn === "" || clockOut === "") {
-    alert("Please enter both clock in and clock out times.");
+  if (clockInOut === "") {
+    alert("Please enter at least one clock in/out pair.");
     return;
   }
   
-  const clockInTime = new Date(`01/01/2000 ${clockIn}`);
-  const clockOutTime = new Date(`01/01/2000 ${clockOut}`);
-  let timeDiff = clockOutTime.getTime() - clockInTime.getTime();
+  const times = clockInOut.trim().split(/[\n\r]+/);
+  let totalTime = 0;
   
-  if (timeDiff < 0) {
-    timeDiff += 86400000;
+  for (let i = 0; i < times.length; i += 2) {
+    const clockIn = new Date(`01/01/2000 ${times[i]}`);
+    const clockOut = new Date(`01/01/2000 ${times[i + 1]}`);
+    let timeDiff = clockOut.getTime() - clockIn.getTime();
+    
+    if (timeDiff < 0) {
+      timeDiff += 86400000;
+    }
+    
+    totalTime += timeDiff;
   }
   
-  const hours = Math.floor(timeDiff / 3600000);
-  const minutes = Math.floor((timeDiff % 3600000) / 60000);
-  const leaveTime = new Date(clockOutTime.getTime() + 1800000); // Add 30 minutes to the clock out time
+  const grossHours = totalTime / 3600000;
+  const effectiveHours = grossHours - 1;
   
-  timeToLeave.value = `${hours} hours and ${minutes} minutes (${leaveTime.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })})`;
+  if (effectiveHours < 8.5) {
+    timeToLeave.value = `Effective hours: ${effectiveHours.toFixed(2)} (less than 8.5 hours).`;
+  } else if (grossHours < 9.5) {
+    timeToLeave.value = `Gross hours: ${grossHours.toFixed(2)} (less than 9.5 hours).`;
+  } else {
+    const now = new Date();
+    const timeToGo = new Date(now.getTime() + ((9.5 - grossHours) * 3600000));
+    const remainingHours = (timeToGo.getTime() - now.getTime()) / 3600000;
+    
+    timeToLeave.value = `Effective hours: ${effectiveHours.toFixed(2)}\nGross hours: ${grossHours.toFixed(2)}\nYou can leave at ${timeToGo.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}. Remaining hours: ${remainingHours.toFixed(2)}.`;
+  }
 }
